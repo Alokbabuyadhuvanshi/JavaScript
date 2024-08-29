caae21cf8d0a08012655750022c0151918ac994c
Alokbabuyadhuvanshi <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-08-29 00:58:17 +0000

    datatypes

--- variableAndconstant/first.js
+++ variableAndconstant/first.js
@@ -1,1 +1,18 @@
-console.log("Alok Babu")
+const accountId = 1345
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "GN"
+let accountState;
+
+//accountId = 2 - not allowed
+
+accountEmail = "abcd@gmail.com"
+accountPassword = "123456"
+accountCity = "dadri"
+
+console.log(accountId);
+
+/* prefer not to use var
+because of issue in block scope and functional scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
