aa68b2f25e96962af9851fc3dacaebbbd077bf12
Alokbabuyadhuvanshi <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-09-01 02:02:17 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name = "Alok"
+const repoCount = 11 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+// In JavaScript, backticks(``) are used for template literals, which allow embedding expressions inside strings
+
+console.log(name[0]);
