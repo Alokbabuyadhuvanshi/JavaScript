314ed1b0059b1b968b530b08a1e9f3bdd401312f
Alok Babu <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-09-04 16:09:30 +0000

    json  talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,7 +5,7 @@ const tinderUser = new Object()   // singleton object
 tinderUser.id = "1234"
 tinderUser.name ="tommy"
 tinderUser.isLoggedIn = false
-console.log(tinderUser);
+//console.log(tinderUser);
 
 const regularUser = {
     email: "alok@gmail.com",
@@ -17,8 +17,8 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname);
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 ={1: "a", 2: "b"}
 const obj2 ={3: "c", 4: "d"}
@@ -28,19 +28,58 @@ const obj2 ={3: "c", 4: "d"}
 //const obj3 = Object.assign(obj1,obj2)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
-console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));
 
-console.log(Object.values(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 
-console.log(tinderUser.hasOwnProperty('name'));
+// console.log(tinderUser.hasOwnProperty('name'));
 
+
+//********* De-structuring **************************************/
+
+const course ={
+    coursename : "javaScript",
+    price : 999,
+    courseInstructor : "alok"
+}
+
+// course.courseInstructor
+
+/*Default Variable Name: If you use the destructuring pattern { courseInstructor }, the variable name will be the same as the property name.
+Custom Variable Name: If you want to use a different name, you can use the aliasing syntax { courseInstructor: newVariableName }.
+*/
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+//         API
+
+// {
+//     "name": "Alok",
+//     "team": "rocket",
+//     "price": 10000
+// }
+
+// json file data
+
+[
+    {},
+    {},
+    {}
+]
 
