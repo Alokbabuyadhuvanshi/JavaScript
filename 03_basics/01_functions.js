22fb6f9acbaf8f6ccc21a6a26806b0d43341c56d
Alok Babu <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-09-04 16:24:12 +0000

    function starting

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+console.log("A")
+console.log("l")
+console.log("o")
+console.log("k")
+
+
+function sayMyName(){
+    console.log("A")
+    console.log("l")
+    console.log("o")
+    console.log("k")
+}
+
+//sayMyName()
+
+function addTwoNumber(num1 , num2){
+    console.log(num1 + num2)
+}
+
+//addTwoNumber(3,"4")
+addTwoNumber(3,null)
