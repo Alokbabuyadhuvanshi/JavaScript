0f883b182c5d8ffb6851f2b92c5cae4054d0a262
Alok Babu <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-09-05 16:34:37 +0000

    globle and block scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,19 @@
-let a = 10
-const b = 20
-var c = 30
+//globle scope |    block scope
 
-console.log(a);
-console.log(b);
+let a = 10
+// const b = 20
+// var c = 30
+
+if(true){
+    let a = 100
+    const b =20
+    var c =30
+    console.log("inner a",a);
+} 
+
+
+console.log("outer a",a);  //this will show the error
+// console.log(b);      // this also show the error
 console.log(c);
+
+
