c7d62303ddc5f0e585ef652a7229cf6becd3b184
Alok Babu <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-09-06 01:54:35 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,19 +1,49 @@
-//globle scope |    block scope
+//globle scope > block scope
 
 let a = 10
 // const b = 20
 // var c = 30
 
-if(true){
-    let a = 100
-    const b =20
-    var c =30
-    console.log("inner a",a);
-} 
+// if(true){
+//     let a = 100
+//     const b =20
+//     var c =30
+//     console.log("inner a",a);
+// } 
 
 
-console.log("outer a",a);  //this will show the error
+//console.log("outer a",a);  //this will show the error
 // console.log(b);      // this also show the error
-console.log(c);
+//console.log(c);
 
 
+function one(){
+    const username = "alok"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+
+    two()
+}
+one()
+
+
+// ++++++++++++++++++++++++Interesting ++++++++++++++++++++++++
+
+function addone(num){
+    return num+1         // we can access this function at any place in file 
+}
+
+console.log(addone(3))
+
+//addtwo(4)  // this will show an error
+
+const addtwo = function(num){    // these type of function we known as expression , it is a type of variable "addtwo"
+            return num+2       
+            }                      // but here we can't acces this function at any place in the file , you can only access in the downside at the point of declaration
+
+console.log(addtwo(4))
