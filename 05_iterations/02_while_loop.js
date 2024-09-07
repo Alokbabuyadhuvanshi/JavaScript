c91a33f427c9a1e4aa1ff4f36e4dd0cf3e18fb36
Alok Babu <124550479+Alokbabuyadhuvanshi@users.noreply.github.com>
2024-09-07 13:44:51 +0000

    while loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,16 @@
+
+// While loop
+i=1
+while (i<10) {
+    console.log(`${i} is less than 10`);
+    i++
+}
+
+
+// Do - While loop
+
+j=0;
+do {
+    console.log(`${j} still less than 10`)
+    j++
+} while (j<10);
